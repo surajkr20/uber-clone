001d931f46242fe64e6884b67657aef11d762f16
surajkr20 <suraj71308kumar@gmail.com>
2024-11-30 15:08:23 +0530

    user services file add for creating users

---
+++ backend/services/user.services.js
@@ -0,0 +1,18 @@
+// this file work only creating users
+
+const userModel = require('../model/user.model');
+
+module.exports.createUser = async ({firstname, lastname, email, password}) =>{
+    if(!firstname || !lastname || !email || !password){
+        throw new Error('All fields are required')
+    }
+    const user = userModel.create({
+        fullname: {
+            firstname,
+            lastname
+        },
+        email,
+        password
+    })
+    return user
+}
