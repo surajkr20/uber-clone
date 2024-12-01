9877a2b9cf368412bcef9879afb7ff6bc3ed2401
surajkr20 <suraj71308kumar@gmail.com>
2024-12-01 13:50:33 +0530

    user schema's all functions in try catch bloch

--- backend/model/user.model.js
+++ backend/model/user.model.js
@@ -1,4 +1,3 @@
-
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
@@ -8,41 +7,52 @@ const userSchema = new mongoose.Schema({
         firstname: {
             type: String,
             required: true,
-            minlength: [ 3, 'First name must be at least 3 characters long' ],
+            minlength: [3, 'First name must be at least 3 characters long'],
         },
         lastname: {
             type: String,
-            minlength: [ 3, 'Last name must be at least 3 characters long' ],
+            minlength: [3, 'Last name must be at least 3 characters long'],
         }
     },
     email: {
         type: String,
         required: true,
+        unique: true, // Ensures email uniqueness
     },
     password: {
         type: String,
         required: true,
+        select: false, // Prevents the password field from being selected in queries by default
     },
     socketId: {
-        type: String
+        type: String,
     }
-})
+});
 
-// token generation
-userSchema.methods.generateAuthToken = () =>{
-    const token = jwt.sign({_id: this._id}, process.env.JWT_SECRET);
+// Token generation
+userSchema.methods.generateAuthToken = function () {
+    const token = jwt.sign({ _id: this._id }, process.env.JWT_SECRET, { expiresIn: '24h' }); // Expiry added for security
     return token;
-}
+};
 
-// comparing password using bcrypt
-userSchema.methods.comparePassword = async () =>{
-    return await bcrypt.compare(password, this.password)
-}
+// Comparing password using bcrypt
+userSchema.methods.comparePassword = async function (password) {
+    try {
+        return await bcrypt.compare(password, this.password);
+    } catch (error) {
+        throw new Error('Error comparing passwords');
+    }
+};
 
-// password hash
-userSchema.methods.hashedPassword = async () =>{
-    return await bcrypt.hash(password, 395)
-}
+// Password hash
+userSchema.methods.hashedPassword = async function (password) {
+    try {
+        const saltRounds = 10; // Standard salt rounds
+        return await bcrypt.hash(password, saltRounds);
+    } catch (error) {
+        throw new Error('Error hashing password');
+    }
+};
 
-const userModel = mongoose.model("User", userSchema)
-module.exports = userModel
+const userModel = mongoose.model('User', userSchema);
+module.exports = userModel;
