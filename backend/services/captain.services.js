c415b10cb4c9a789779856454d1074848ee8ec5e
surajkr20 <suraj71308kumar@gmail.com>
2024-12-02 14:59:17 +0530

    captain sevice add

---
+++ backend/services/captain.services.js
@@ -0,0 +1,21 @@
+
+const captainModel = require('../model/captain.Model');
+
+module.exports.createCaptain = async ({firstname, lastname, email, password, color, plate, capacity, vehicleType}) =>{
+    if(!firstname || !lastname || !email || !password ||!color || !plate || !capacity || !vehicleType ){
+        throw new Error('All fields are required')
+    }
+    const captain = captainModel.create({
+        fullname: {
+            firstname,
+            lastname
+        },
+        email,
+        password,
+        color,
+        plate,
+        capacity,
+        vehicleType
+    })
+    return captain;
+}
