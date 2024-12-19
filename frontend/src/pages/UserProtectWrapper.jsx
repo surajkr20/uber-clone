5a5acd964a56c523397d02a3408a6ca1f4fa2306
surajkr20 <suraj71308kumar@gmail.com>
2024-12-19 05:41:10 +0530

    logout navigation done

--- frontend/src/pages/UserProtectWrapper.jsx
+++ frontend/src/pages/UserProtectWrapper.jsx
@@ -1,16 +1,17 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
-import React from "react";
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 const UserProtectWrapper = ({ children }) => {
-
   const token = localStorage.getItem("token");
   const navigate = useNavigate();
 
-  if (!token) {
-    navigate("/login");
-  }
+  useEffect(() => {
+    if (!token) {
+      navigate("/login");
+    }
+  }, [navigate, token]);
 
   return <>{children}</>;
 };
