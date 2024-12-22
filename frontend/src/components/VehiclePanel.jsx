8b9dd0a88f0ec4baaf0203a3b4e6978eb2f030a1
surajkr20 <suraj71308kumar@gmail.com>
2024-12-22 07:47:48 +0530

    vehicle panle done

---
+++ frontend/src/components/VehiclePanel.jsx
@@ -0,0 +1,83 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+const VehiclePanel = (props) => {
+  return (
+    <div>
+        <div className="flex items-center justify-between p-2">
+          <h2 className="text-2xl font-semibold mb-2">Choose your Vehicle</h2>
+          <h5 onClick={()=>{
+            props.setVehiclePanelOpen(false)
+          }} className="text-3xl font-semibold mb-2">
+            <i className="ri-arrow-down-wide-fill"></i>
+          </h5>
+        </div>
+
+        <div className="flex items-center justify-center gap-4 w-full p-3 border-2 active:border-black rounded-lg mb-3">
+          <img
+            src="https://www.svgrepo.com/show/408292/car-white.svg"
+            alt="uber/car/img"
+            className="h-20"
+          />
+          <div className="w-1/2 ml-2">
+            <h4 className="font-medium text-xl">
+              UberGo{" "}
+              <span>
+                <i className="ri-user-3-fill"></i>4
+              </span>
+            </h4>
+            <h5 className="font-medium text-sm">2 mins, Away</h5>
+            <p className="font-medium text-xs">Affordable, compact rides</p>
+          </div>
+          <h2 className="text-xl font-semibold">
+            <i className="ri-money-rupee-circle-line"></i>192.20
+          </h2>
+        </div>
+
+        <div className="flex items-center justify-center gap-4 w-full p-3 border-2 active:border-black rounded-lg mb-3">
+          <img
+            src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_368,w_552/v1649231091/assets/2c/7fa194-c954-49b2-9c6d-a3b8601370f5/original/Uber_Moto_Orange_312x208_pixels_Mobile.png"
+            alt="uber/car/img"
+            className="h-14"
+          />
+          <div className="w-1/2 ml-2">
+            <h4 className="font-medium text-xl">
+              Moto{" "}
+              <span>
+                <i className="ri-user-3-fill"></i>3
+              </span>
+            </h4>
+            <h5 className="font-medium text-sm">3 mins, Away</h5>
+            <p className="font-medium text-xs">Affordable, MotorCycle rides</p>
+          </div>
+          <h2 className="text-xl font-semibold">
+            <i className="ri-money-rupee-circle-line"></i>65
+          </h2>
+        </div>
+
+        <div className="flex items-center justify-center gap-4 w-full p-3 border-2 active:border-black rounded-lg">
+          <img
+            src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_368,w_552/v1648431773/assets/1d/db8c56-0204-4ce4-81ce-56a11a07fe98/original/Uber_Auto_558x372_pixels_Desktop.png"
+            alt="uberauto"
+            className="h-14"
+          />
+          <div className="w-1/2 ml-2">
+            <h4 className="font-medium text-xl">
+              Moto{" "}
+              <span>
+                <i className="ri-user-3-fill"></i>1
+              </span>
+            </h4>
+            <h5 className="font-medium text-sm">3 mins, Away</h5>
+            <p className="font-medium text-xs">Affordable, Auto rides</p>
+          </div>
+          <h2 className="text-xl font-semibold">
+            <i className="ri-money-rupee-circle-line"></i>118.50
+          </h2>
+        </div>
+    </div>
+  )
+}
+
+export default VehiclePanel
